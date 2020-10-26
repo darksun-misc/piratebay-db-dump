b878c4b95baf1e4ac5cdf0e3a1758377ead110db
Artur Klesun ECMA God <arturklesun@gmail.com>
2020-10-27 01:30:45 +0200

    Add static page with simple text search for torrents to be published on github pages

---
+++ web_client/Dom.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} tagName = 'div' || 'span' || 'li'
+ * @param {Record<string, string|number|function>} attributes
+ * @param {HTMLElement[]|string} children
+ * @return {HTMLElement}
+ */
+export const Dom = (tagName, attributes = {}, children = []) => {
+    const dom = document.createElement(tagName);
+    for (const [name, value] of Object.entries(attributes)) {
+        if (['string', 'number'].includes(typeof value)) {
+            dom.setAttribute(name, value);
+        } else {
+            dom[name] = value;
+        }
+    }
+    if (['string', 'number'].includes(typeof children)) {
+        dom.textContent = children;
+    } else {
+        for (const child of children) {
+            dom.appendChild(child);
+        }
+    }
+    return dom;
+};
